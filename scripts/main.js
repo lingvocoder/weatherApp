60a09eefec9e9e122b28417bdcb4ba43bd0d5aad
Aleksey Goncharov <to-english@yandex.ru>
2022-08-30 23:35:12 +0300

    Rafactor styles for namespace .weather

--- scripts/main.js
+++ scripts/main.js
@@ -19,7 +19,7 @@ class WeatherApp {
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"/>
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"/>
                                 </svg>
-                                <span class="weather__text"></span>
+                                <span class="weather__heading"></span>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="menu__icon menu__icon_open">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 011.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.56.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.893.149c-.425.07-.765.383-.93.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 01-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.397.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 01-.12-1.45l.527-.737c.25-.35.273-.806.108-1.204-.165-.397-.505-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.107-1.204l-.527-.738a1.125 1.125 0 01.12-1.45l.773-.773a1.125 1.125 0 011.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894z"/>
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"/>
@@ -27,14 +27,14 @@ class WeatherApp {
                             </div>
                             <main class="weather-content">
                                 <div class="weather-content__inner">
-                                    <div class="weather-content__row weather-content__row_main">
-                                        <span class="weather-content__elem weather-content__elem_icon">
-                                            <i class="wi wi-des weather-content__icon weather-content__icon_main"></i>
+                                    <div class="weather-content__row">
+                                        <span id="mainIcon" class="weather-content__elem">
+                                            <i class="wi weather-content__icon_main"></i>
                                         </span>
-                                        <span class="weather-content__elem weather-content__elem_des"></span>
-                                        <span class="weather-content__elem weather-content__elem_deg"></span>
+                                        <span id="description" class="weather-content__elem"></span>
+                                        <span id="degree" class="weather-content__elem"></span>
                                     </div>
-                                    <div class="weather-content__row weather-content__row_supp">
+                                    <div class="weather-content__row">
                                         <span id="wind" class="weather-content__elem weather-content__elem_supp"></span>
                                         <span id="humidity" class="weather-content__elem weather-content__elem_supp"></span>
                                         <span id="pressure" class="weather-content__elem weather-content__elem_supp"></span>
@@ -47,7 +47,7 @@ class WeatherApp {
                                 <div class="menu__wrapper">
                                     <div class="menu__inner">
                                         <div class="weather__header">
-                                            <span class="weather__text">Settings</span>
+                                            <span class="weather__heading">Settings</span>
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="menu__icon menu__icon_close">
                                                 <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 19.5l15-15m-15 0l15 15"/>
                                             </svg>
@@ -444,12 +444,12 @@ class WeatherApp {
     }
 
     displayLocation = (city, country) => {
-        let node = document.querySelector(".weather__text");
+        let node = document.querySelector(".weather__heading");
         node.textContent = `${city}, ${country}`;
     }
 
     setTemperature = (temperature) => {
-        const node = document.querySelector(".weather-content__elem_deg");
+        const node = document.querySelector("#degree");
         let na = document.createElement("i");
         na.classList.add("wi", "wi-na");
 
@@ -462,7 +462,8 @@ class WeatherApp {
     }
 
     setIconCode = (icon, id) => {
-        const node = document.querySelector(".wi.weather-content__icon_main");
+        const node = document.querySelector("#mainIcon");
+        node.className = 'weather-content__icon_main';
         if (icon === undefined) {
             node.classList.add("wi-na");
         } else if (icon.indexOf("n") !== -1) {
@@ -473,7 +474,7 @@ class WeatherApp {
     }
 
     setDescription = (des, feels) => {
-        let description = document.querySelector(".weather-content__elem_des");
+        let description = document.querySelector("#description");
         let na = document.createElement("i");
         na.classList.add("wi", "wi-na");
 
