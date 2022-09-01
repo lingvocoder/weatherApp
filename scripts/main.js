001876240354c2153dadad12b1da06ccb26c4607
Aleksey Goncharov <to-english@yandex.ru>
2022-09-01 07:09:11 +0300

    Rename menu item textNode className

--- scripts/main.js
+++ scripts/main.js
@@ -195,7 +195,7 @@ class WeatherApp {
                      viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z"/>
                 </svg>
-                <span class="menu__fetch">${resString}</span>
+                <span class="menu__text">${resString}</span>
                 <svg class="menu__icon menu__icon_delete" xmlns="http://www.w3.org/2000/svg" fill="none"
                     viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"/>
@@ -239,12 +239,12 @@ class WeatherApp {
             const {target} = ev;
             const deleteButton = target.closest('.menu__icon_delete');
             if (!deleteButton) return;
-            const currItem = target.closest("[draggable='true']");
+            const currItem = target.closest(".menu__item");
             const {id: currItemID} = currItem.dataset;
             let cachedList = that.getCachedList();
             let listCopy = [...cachedList];
             const filteredList = listCopy.filter(item => item.id !== parseInt(currItemID));
-            list.removeChild(currItem);
+            list.removeChild(currItem.parentNode);
             that.updateList(filteredList);
 
         },true)
@@ -293,7 +293,7 @@ class WeatherApp {
         list.addEventListener('click', (ev) => {
             ev.stopPropagation();
             const target = ev.target;
-            const menuItem = target.closest('.menu__fetch');
+            const menuItem = target.closest('.menu__text');
             const {location} = menuItem.parentNode.dataset;
             const {city, country} = JSON.parse(location);
             const menu = `.menu`;
